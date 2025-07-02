7224970190cf273e80adf8dd52ada845c0ca90bb
TVu-TVu <tvutrabaja@gmail.com>
2025-07-02 21:49:40 +0700

    re-update

---
+++ app/src/main/java/com/example/myapplication2/main.java
@@ -0,0 +1,17 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import  android.view.View;
+
+public class main extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
